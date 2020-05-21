acc2b4bad6753e2294e70c0ae4c2288c8871a9a7
Yuri Matheus <yurimatheusb@hotmail.com>
2020-05-21 15:44:19 -0300

    project setup

---
+++ src/main/java/me/oyurimatheus/nossoservicodepagamento/NossoServicoDePagamentoApplication.java
@@ -0,0 +1,13 @@
+package me.oyurimatheus.nossoservicodepagamento;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NossoServicoDePagamentoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NossoServicoDePagamentoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/me/oyurimatheus/nossoservicodepagamento/NossoServicoDePagamentoApplicationTests.java
@@ -0,0 +1,13 @@
+package me.oyurimatheus.nossoservicodepagamento;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NossoServicoDePagamentoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
