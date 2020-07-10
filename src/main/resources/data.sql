a72d76911736b10993f6753d7dcb5abf48325398
Yuri Matheus <yurimatheusb@hotmail.com>
2020-07-10 14:40:38 -0300

    make the test data

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -34,3 +34,32 @@ INSERT INTO restaurants_payment_methods(payment_method, restaurant_id) VALUES ('
 INSERT INTO restaurants_payment_methods(payment_method, restaurant_id) VALUES ('CASH', 4);
 INSERT INTO restaurants_payment_methods(payment_method, restaurant_id) VALUES ('CASH', 5);
 INSERT INTO restaurants_payment_methods(payment_method, restaurant_id) VALUES ('CHECK', 5);
+
+
+INSERT INTO payment_gateways (payment_gateway_id, gateway_name, fare, fare_type, gateway_uri, country) VALUES (1, 'Hyoga', 4, 'CASH', 'http://localhost:8080/gateways/pay', 'BR');
+INSERT INTO payment_gateways (payment_gateway_id, gateway_name, fare, fare_type, gateway_uri, country) VALUES (2, 'Saori', 0.01, 'PERCENTAGE', 'http://localhost:8080/gateways/pay', 'BR');
+INSERT INTO payment_gateways (payment_gateway_id, gateway_name, fare, fare_type, gateway_uri, country) VALUES (3, 'Seya', 5, 'CASH', 'http://localhost:8080/gateways/pay', 'BR');
+INSERT INTO payment_gateways (payment_gateway_id, gateway_name, fare, fare_type, gateway_uri, country) VALUES (4, 'Shiryu', 3.5, 'CASH', 'http://localhost:8080/gateways/pay', 'BR');
+INSERT INTO payment_gateways (payment_gateway_id, gateway_name, fare, fare_type, gateway_uri, country) VALUES (5, 'Shun', 4, 'CASH', 'http://localhost:8080/gateways/pay', 'BR');
+INSERT INTO payment_gateways (payment_gateway_id, gateway_name, fare, fare_type, gateway_uri, country) VALUES (6, 'Tango', 5), 'CASH', 'http://localhost:8080/gateways/pay', 'ARG';
+
+INSERT INTO payment_gateways_cards(card_flag, payment_gateway_id) VALUES ('VISA', 1);
+INSERT INTO payment_gateways_cards(card_flag, payment_gateway_id) VALUES ('MASTERCARD', 1);
+INSERT INTO payment_gateways_cards(card_flag, payment_gateway_id) VALUES ('HYPERCARD', 1);
+INSERT INTO payment_gateways_cards(card_flag, payment_gateway_id) VALUES ('VISA', 2);
+INSERT INTO payment_gateways_cards(card_flag, payment_gateway_id) VALUES ('MASTERCARD', 2);
+INSERT INTO payment_gateways_cards(card_flag, payment_gateway_id) VALUES ('HYPERCARD', 2);
+INSERT INTO payment_gateways_cards(card_flag, payment_gateway_id) VALUES ('ELO', 2);
+INSERT INTO payment_gateways_cards(card_flag, payment_gateway_id) VALUES ('VISA', 3);
+INSERT INTO payment_gateways_cards(card_flag, payment_gateway_id) VALUES ('MASTERCARD', 3);
+INSERT INTO payment_gateways_cards(card_flag, payment_gateway_id) VALUES ('HYPERCARD', 3);
+INSERT INTO payment_gateways_cards(card_flag, payment_gateway_id) VALUES ('ELO', 3);
+INSERT INTO payment_gateways_cards(card_flag, payment_gateway_id) VALUES ('VISA', 4);
+INSERT INTO payment_gateways_cards(card_flag, payment_gateway_id) VALUES ('MASTERCARD', 4);
+INSERT INTO payment_gateways_cards(card_flag, payment_gateway_id) VALUES ('VISA', 5);
+INSERT INTO payment_gateways_cards(card_flag, payment_gateway_id) VALUES ('MASTERCARD', 5);
+INSERT INTO payment_gateways_cards(card_flag, payment_gateway_id) VALUES ('ELO', 5);
+INSERT INTO payment_gateways_cards(card_flag, payment_gateway_id) VALUES ('VISA', 6);
+INSERT INTO payment_gateways_cards(card_flag, payment_gateway_id) VALUES ('MASTERCARD', 6);
+INSERT INTO payment_gateways_cards(card_flag, payment_gateway_id) VALUES ('HYPERCARD', 6);
+INSERT INTO payment_gateways_cards(card_flag, payment_gateway_id) VALUES ('ELO', 6);
