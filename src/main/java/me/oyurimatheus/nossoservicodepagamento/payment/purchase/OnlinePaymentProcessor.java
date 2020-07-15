fea309fc5ffbda5f805b261f9395fea6f611b734
Yuri Matheus <yurimatheusb@hotmail.com>
2020-07-15 15:14:28 -0300

    refactor based on cognitive load

--- src/main/java/me/oyurimatheus/nossoservicodepagamento/payment/purchase/OnlinePaymentProcessor.java
+++ src/main/java/me/oyurimatheus/nossoservicodepagamento/payment/purchase/OnlinePaymentProcessor.java
@@ -1,35 +1,26 @@
 package me.oyurimatheus.nossoservicodepagamento.payment.purchase;
 
 import me.oyurimatheus.nossoservicodepagamento.payment.purchase.gateways.PaymentAttempt;
-import me.oyurimatheus.nossoservicodepagamento.payment.purchase.gateways.PaymentGateway;
 import me.oyurimatheus.nossoservicodepagamento.payment.purchase.gateways.PaymentGatewayClient;
 import me.oyurimatheus.nossoservicodepagamento.payment.purchase.gateways.PaymentGatewayRepository;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
-import java.util.Optional;
-
-import static java.util.stream.Collectors.toList;
 
 @Service
 public class OnlinePaymentProcessor {
 
-    private final PaymentGatewayRepository paymentGatewayRepository;
     private final PaymentGatewayClient client;
+    private final PaymentAttemptFactory paymentAttemptFactory;
 
-    public OnlinePaymentProcessor(PaymentGatewayRepository paymentGatewayRepository,
-                                  PaymentGatewayClient client) {
-        this.paymentGatewayRepository = paymentGatewayRepository;
+    public OnlinePaymentProcessor(PaymentGatewayClient client,
+                                  PaymentAttemptFactory paymentAttemptFactory) {
+        this.paymentAttemptFactory = paymentAttemptFactory;
         this.client = client;
     }
 
     public PaymentTransaction tryToPay(Payment payment) {
-        List<PaymentGateway> gateways = paymentGatewayRepository.findAll();
-        List<PaymentAttempt> paymentAttempts = gateways.stream()
-                                                       .map(gateway -> gateway.forPayment(payment))
-                                                       .filter(Optional::isPresent)
-                                                       .map(Optional::get)
-                                                       .collect(toList());
+        List<PaymentAttempt> paymentAttempts = paymentAttemptFactory.makeAttempts(payment);
 
         OnlinePaymentAttemptsOrder paymentOrder = OnlinePaymentAttemptsOrder.makeGatewaysAttemptsOrderTo(client, paymentAttempts);
 
